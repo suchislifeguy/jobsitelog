61a40eb02b0a160e8c82af8486f0dc9235a245ac
suchislifeguy <landercare@gmail.com>
2026-05-03 21:48:28 +1000

    Update and rename metadata.json to sw.js

---
+++ sw.js
@@ -0,0 +1,8 @@
+const CACHE_NAME = 'joblog-pwa-v1';
+const ASSETS_TO_CACHE = [
+  '/jobsitelog/',
+  '/jobsitelog/index.html',
+  '/jobsitelog/manifest.json',
+  '/jobsitelog/icon-192.png',
+  '/jobsitelog/icon-512.png'
+];
